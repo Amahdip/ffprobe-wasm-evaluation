f6f2d4deda86f09615c17665e943d061ccb69eab
Amirmahdi <amp@Amirmahdis-MacBook-Pro.local>
2026-06-15 12:37:11 +0330

    fix: configure optimizeDeps.entries in vite.config.ts to avoid scanning emsdk

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,9 @@ export default defineConfig({
   server: {
     headers: crossOriginIsolationHeaders,
   },
+  optimizeDeps: {
+    entries: ['index.html'],
+  },
   preview: {
     headers: crossOriginIsolationHeaders,
   },
